63abb83ed8359a78e69aacefdf33a178fb90ad48
Millan Philipose <millan@imbue.com>
2026-08-11 14:01:15 -0700

    Fix android linkedin (#111)
    * bump
    
    * fix
    
    * patch
    
    * bump

--- Bouncer_android/app/build.gradle.kts
+++ Bouncer_android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.imbue.bouncer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
+        versionCode = 11
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
