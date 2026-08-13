1b8e0fc9065babb7fffd5dc76de4ee89ac7193d4
Millan Philipose <millan@imbue.com>
2026-08-13 14:26:40 -0700

    bump

--- Bouncer_android/app/build.gradle.kts
+++ Bouncer_android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.imbue.bouncer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.1.0"
+        versionCode = 12
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
