c76813592df90e5e629c7722d08126da52cc9ae2
Millan Philipose <millan@imbue.com>
2026-08-26 17:30:37 -0700

    Millan/mobile bump (#122)
    * bump
    
    * allow filtering of videos and retweets
    
    * bump
    
    * bump mobile

--- Bouncer_android/app/build.gradle.kts
+++ Bouncer_android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.imbue.bouncer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.1.2"
+        versionCode = 15
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
